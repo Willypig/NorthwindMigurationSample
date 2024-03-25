e486a1cbbffc35ee035782661f77e088a5d96574
Willy <willypig93@gmail.com>
2024-03-25 19:44:08 +0800

    seed category data 1 - 4

---
+++ WebApplication1/Migrations/20240325114224_SeedCategoryTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName", "Description", "Picture" },
+                values: new object[,]
+                {
+                    { 1, "Name01", null, null },
+                    { 2, "Name01", null, null },
+                    { 3, "Name01", null, null },
+                    { 4, "Name01", null, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 4);
+        }
+    }
+}
---
+++ WebApplication1/Models/SeedDBData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public partial class NorthwindContext : DbContext
+    {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                CategoryId = 1,
+                CategoryName = "Name01",
+            });
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                CategoryId = 2,
+                CategoryName = "Name01",
+            });
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                CategoryId = 3,
+                CategoryName = "Name01",
+            });
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                CategoryId = 4,
+                CategoryName = "Name01",
+
+            });
+        }
+    }
+}
